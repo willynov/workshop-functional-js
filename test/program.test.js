63d72ca97a68f2184c7646f3f1ed7108747f6a13
GALBY <william.galby@cgi.com>
2017-02-24 09:24:53 +0100

    Exercise 1

--- test/program.test.js
+++ test/program.test.js
@@ -8,5 +8,23 @@ describe('Function transformCheckpoint', function() {
   it('Function transformCheckpoint without parameter should return false', function() {
     expect(transformCheckpoint()).to.be.false;
   });
+  it('Function transformCheckpoint with parameter is mutate', function() {
+    expect(transformCheckpoint({
+      id: 'whataw0nd3rful1d',
+      uuid: 'whataw0nd3rful1d',
+      address: 'unknown',
+      addressType: 'unknown',
+      connectable: true,
+      advertisement: {
+        localName: undefined,
+        txPowerLevel: undefined,
+        manufacturerData: undefined,
+        serviceData: [],
+        serviceUuids: [ 'abcd' ]},
+      rssi: -66,
+      services: null,
+      state: 'outofcontrol'
+    })).to.be.eql(true);
+  });
 
 });
